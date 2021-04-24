1ce42ce9bfd7573c7dde331e23fd5a51c50fa3b7
Dan <danazad@gmail.com>
2021-04-24 00:16:59 -0500

    Units

--- weatherAppSecond.js
+++ weatherAppSecond.js
@@ -1,19 +1,21 @@
+const temperatureSelector = document.querySelector('#temperature-units');
+
+let unit;
+
+unitSelector();
+
+console.log(unit);
+
 let cityName = 'london';
+let countryName = 'uk'
 
-let dataArray = [];
+let apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&appid=469f04c0b3bc1ee6ca83abdfb8c7e6d3&units=metric`;
 
-let apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=469f04c0b3bc1ee6ca83abdfb8c7e6d3&units=metric`;
+let weatherData = [];
 
-const displayWeatherData = (data) => {
-  addingData(data);
-
-  dataArray.push(data.main.temp.toString());
-  dataArray.push(data.main.feels_like.toString());
-  dataArray.push(data.main.temp_min.toString());
-  dataArray.push(data.main.temp_max.toString());
+const functionalites = (data) => {
+  recordingData(data);
 }
-
-console.log(dataArray);
 
 const graphing = () => {
   let ctx = document.getElementById('chart').getContext('2d');
@@ -23,18 +25,18 @@ const graphing = () => {
           labels: ['Temperature', 'Feels Like', 'Maximum Temperature', 'Minimum Temperature'],
           datasets: [{
               label: 'Temperature',
-              data: dataArray,
+              data: [],
               backgroundColor: [
-                  'rgba(255, 99, 132, 0.5)',
-                  'rgba(54, 162, 235, 0.5)',
-                  'rgba(255, 206, 86, 0.5)',
-                  'rgba(75, 192, 192, 0.5)'
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)'
               ],
               borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
               ],
               borderWidth: 2.5
           }]
@@ -49,12 +51,13 @@ const graphing = () => {
   });
 }
 
-graphing();
-
-const weatherApi = async (apiLink) => {
+const weatherApi = async (apiLink) => {  
   const responseFlow = await fetch(apiLink);
   const data = await responseFlow.json();
-  displayWeatherData(data);
+  functionalites(data);
+
+  weatherData.slice(1, weatherData.length);
+  weatherData.push(data.main.temp);
 }
 
 weatherApi(apiLink);
@@ -70,30 +73,43 @@ document.addEventListener('click', (event) => {
       break;
   }
 })
-
 document.addEventListener('keyup', (event) => {
   const elementId = event.key;
 
   switch (elementId) {
     case "Enter":
-      cityName = document.querySelector('#location').value;
-      apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=469f04c0b3bc1ee6ca83abdfb8c7e6d3&units=metric`;
+      cityName = document.querySelector('#city-name').value;
+      countryName = document.querySelector('#country-name').value;
+      apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&appid=469f04c0b3bc1ee6ca83abdfb8c7e6d3&units=metric`;
       weatherApi(apiLink);
       break;
   }
 })
 
-function addingData(data) {
+// Helper function to give values to the HTML elements
+function recordingData(data) {
   document.querySelector('object').setAttribute('data', `/animated/${data.weather[0].icon}.svg`)
   selectElementTemplate('main', data.weather[0].main);
   selectElementTemplate('description', data.weather[0].description);
-  selectElementTemplate('temperature', data.main.temp);
-  selectElementTemplate('feels-like', data.main.feels_like);
-  selectElementTemplate('minimum-temperature', data.main.temp_min);
-  selectElementTemplate('maximum-temperature', data.main.temp_max);
+  selectElementTemplate('temperature', `${data.main.temp} ${unit}`);
+  selectElementTemplate('feels-like', `${data.main.feels_like} ${unit}`);
+  selectElementTemplate('minimum-temperature', `${data.main.temp_min} ${unit}`);
+  selectElementTemplate('maximum-temperature', `${data.main.temp_max} ${unit}`);
   selectElementTemplate('humidity', data.main.humidity);
 }
 
 function selectElementTemplate(elementId, value) {
   return document.querySelector(`#${elementId}`).textContent = value;
+}
+
+function unitSelector() {
+  if (temperatureSelector.value === 'celsius') {
+    unit = '°C'
+  } else if (temperatureSelector.value === 'fahrenheit') {
+    unit = '°F'
+  } else if (temperatureSelector.value === 'kelvin') {
+    unit = 'K'
+  }
+
+  return unit
 }
